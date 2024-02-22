77bf69c1934adcd59f16678417ace022931108b2
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-02-22 16:29:11 +0700

    Menambahkan route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>PWL 2024</title>
+        <title>PWL 2024 ALVIA</title>
     </head>
     <body>
         <h2>Pemrograman Web lanjut 2024</h2>
         <h3>HALLO VIA</h3>
+        <h5>Selamat makan siang wkwk</h5>
     </body>
 </html>
