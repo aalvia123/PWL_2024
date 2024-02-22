3b59690037225c7af4ea9da45d5047c0079aeeb6
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-02-22 21:14:29 +0700

    Menambahkan route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hello', function () {
+ return 'Hello World';
+});
+Route::get('/world', function () {
+    return 'World';
+   });
+
+   Route::get('/', function () {
+    return 'Selamat Datang';
+   });
+
+   Route::get('/about', function () {
+    return 'ALVIA NIM 2341728011';
+   });
+
+   Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya '.$name;
+    });
+
+    Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/Id/{Id}/comments/{comment}', function
+($Id, $commentId) {
+ return ' Halaman Artikel ' .$Id." Dengan ID : ".$commentId;
+});
+
+
+Route::get('/user/{name?}', function ($name='john') {
+    return 'Nama saya '.$name;
+});
+
+
+
+
+
+
